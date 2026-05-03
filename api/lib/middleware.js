bb433070ad07de8a6350a0912eab29c7d031a823
Deladem Avudzega <avuddelkod@gmail.com>
2026-05-03 21:42:33 +0000

    Restrict CORS to specific origin

--- api/lib/middleware.js
+++ api/lib/middleware.js
@@ -12,7 +12,7 @@ function getSupabase() {
 }
 
 function cors(res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://biztrackv1.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 }
